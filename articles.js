e2750f0eb215eb775e54396ad57aab5a95fac5f4
Javier <javier.merida@mykukun.com>
2021-05-16 18:49:24 +0200

    big update and fixes, adding community boxes and more

--- articles.js
+++ articles.js
@@ -4,7 +4,7 @@ var multiplier = 1;
 
 [...containers].forEach(container => {
     container.addEventListener('wheel', function(e) {
-        e.preventDefault();
+        //e.preventDefault();
         let dir = (e.deltaY > 0)? 'right' : 'left';
         container.horizontalScroll({
             speed: 15,
@@ -12,9 +12,34 @@ var multiplier = 1;
             interval: 25,
             direction: dir
         })
+
+        let scrollLimit = container.scrollWidth - container.offsetWidth;
+        let isGoingLeftUnderLimit = (container.scrollLeft > 0  && dir && dir === 'left');
+        let isGoingRightUnderLimit = (container.scrollLeft < scrollLimit && dir === 'right');
+        if(isGoingLeftUnderLimit || isGoingRightUnderLimit)
+            e.preventDefault();
     });
 })
 
+containers = document.getElementsByClassName('posts-container');
+
+[...containers].forEach(container => {
+    container.addEventListener('wheel', function(e) {
+        let dir = (e.deltaY > 0)? 'right' : 'left';
+        container.horizontalScroll({
+            speed: 15,
+            scrollAmount: SCROLL_AMOUNT,
+            interval: 25,
+            direction: dir
+        })
+
+        let scrollLimit = container.scrollWidth - container.offsetWidth;
+        let isGoingLeftUnderLimit = (container.scrollLeft > 0  && dir && dir === 'left');
+        let isGoingRightUnderLimit = (container.scrollLeft < scrollLimit && dir === 'right');
+        if(isGoingLeftUnderLimit || isGoingRightUnderLimit)
+            e.preventDefault();
+    });
+})
 
 /**
  * @param config {Object}
